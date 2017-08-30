177fae224e5d11618d0621e92611a7a830f7ef52
Aldwin Delgado <aldwinpaolodelgado@gmail.com>
2017-08-30 19:00:43 +0800

    Update oauth server sample

---
+++ src/main/java/com/github/oauth/sample/AuthorizationServerConfig.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.oauth.sample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+/**
+ *
+ * @author Aldwin Delgado
+ */
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    private TokenStore tokenStore;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .authenticationManager(authenticationManager)
+                .approvalStoreDisabled()
+                .tokenStore(tokenStore);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("account")
+                .secret("password")
+                .authorizedGrantTypes("client_credentials", "password")
+                .scopes("read", "write")
+                .accessTokenValiditySeconds(60)
+                .resourceIds("sample-oauth");
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security
+                .checkTokenAccess("isAuthenticated()")
+                .allowFormAuthenticationForClients();
+    }
+
+}
---
+++ src/main/java/com/github/oauth/sample/JsonToUrlEncodedAuthenticationFilter.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.oauth.sample;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.io.ByteStreams;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.web.savedrequest.Enumerator;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ *
+ * @author Aldwin Delgado
+ */
+@Component
+public class JsonToUrlEncodedAuthenticationFilter extends OncePerRequestFilter {
+
+    /**
+     * Dito pwede nating maalis yung grant type Basically they should be all
+     * client_credentials Append nalang dito yung grant types para hindi masama
+     * sa harapan
+     *
+     * @param request
+     * @param response
+     * @param filterChain
+     * @throws javax.servlet.ServletException
+     * @throws java.io.IOException
+     *
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        if (Objects.equals(request.getServletPath(), "/oauth/token") && Objects.equals(request.getContentType(), "application/json")) {
+
+            byte[] json = ByteStreams.toByteArray(request.getInputStream());
+
+            Map<String, String> jsonMap = new ObjectMapper().readValue(json, Map.class);
+//            jsonMap.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEach((x) -> LOGGER.info("Shit: ", x));
+            Map<String, String[]> parameters
+                    = jsonMap.entrySet().stream()
+                    .collect(Collectors.toMap(
+                                    Map.Entry::getKey,
+                                    e -> new String[]{e.getValue()})
+                    );
+
+            HttpServletRequest requestWrapper = new RequestWrapper(request, parameters);
+            filterChain.doFilter(requestWrapper, response);
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+
+    private class RequestWrapper extends HttpServletRequestWrapper {
+
+        private final Map<String, String[]> params;
+
+        RequestWrapper(HttpServletRequest request, Map<String, String[]> params) {
+            super(request);
+            this.params = params;
+        }
+
+        @Override
+        public String getParameter(String name) {
+            if (this.params.containsKey(name)) {
+                return this.params.get(name)[0];
+            }
+            return "";
+        }
+
+        @Override
+        public Map<String, String[]> getParameterMap() {
+            return this.params;
+        }
+
+        @Override
+        public Enumeration<String> getParameterNames() {
+            return new Enumerator<>(params.keySet());
+        }
+
+        @Override
+        public String[] getParameterValues(String name) {
+            return params.get(name);
+        }
+    }
+
+}
